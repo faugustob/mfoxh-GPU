d48074d773ef5814101f07d0a69dfdd0482b0dff
HatimChergui <35112379+HatimChergui@users.noreply.github.com>
2024-05-06 17:14:23 +0200

    Update Tests.m

--- Tests.m
+++ Tests.m
@@ -7,7 +7,7 @@ Alphap = [1 ; 1 ; 1];
 bq = [2];
 Betaq = [1 ; 1 ; 1];
 Contour = mfoxcontour(10, 3, an, Alphan, [],[0;1],[],[3;1],[],[1;1]);
-H1 = mfoxh(z, Contour, an, Alphan, ap, Alphap, bq, Betaq,[],[],[0;1],[],[],[],[3;1],[],[],[],[1;1],[])
+H1 = mfoxh(z, Contour.', an, Alphan, ap, Alphap, bq, Betaq,[],[],[0;1],[],[],[],[3;1],[],[],[],[1;1],[])
 
 % H1 = 0.4886 + 0.0035i
 
@@ -20,5 +20,5 @@ Alphap = [1 ; 1];
 bq = [2];
 Betaq = [1 ; 1];
 Contour = [-1.5-10i -1.5+10i ; 2.5-10i 2.5+10i]; % contour set manually
-H2 = mfoxh(z, Contour, an, Alphan, ap, Alphap, bq, Betaq,[],[],[0;1],[],[],[],[3;1],[])
+H2 = mfoxh(z, Contour.', an, Alphan, ap, Alphap, bq, Betaq,[],[],[0;1],[],[],[],[3;1],[])
 % H2 = -0.6014 + 0.0011i
